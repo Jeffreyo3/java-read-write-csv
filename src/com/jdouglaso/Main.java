09ee362a08eb081d8947e4b3f729e3d1cd17a2c0
jeffreyo3 <jeffrey.orndorff@gmail.com>
2021-04-13 14:03:23 -0700

    Init with CSV

---
+++ src/com/jdouglaso/Main.java
@@ -0,0 +1,17 @@
+package com.jdouglaso;
+
+import java.io.*;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+
+    }
+
+
+
+
+
+}
